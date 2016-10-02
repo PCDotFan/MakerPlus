9e1003201d292e5d4330fb7bec21bab081dc81c8
PCDotFan <467055732@qq.com>
2016-10-02 18:36:41 +0800

    Add description meta

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,11 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@section('title')Laravel China 社区 - 靠谱的 Laravel 和 PHP 开发者社区@show</title>
+    <title>@section('title')MakerPlus - 严谨而开放的创客交流社区@show</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <meta name="keywords" content="php,laravel,php论坛,laravel论坛,php社区,laravel社区,laravel教程,php教程,laravel视频,php开源,php新手,php7,laravel5" />
     <meta name="author" content="MakerPlus" />
-    <meta name="description" content="@section('description') Laravel China 是国内最大的 Laravel 和 PHP 中文社区，致力于推动 Laravel，PHP7、php-fig 等 PHP 新技术，新理念在中国的发展，是国内最靠谱的 PHP 论坛。 @show" />
+    <meta name="description" content="@section('description') @show" />
     <meta name="_token" content="{{ csrf_token() }}">
     <!--<link rel="stylesheet" href="{{ elixir('assets/css/main.css') }}">-->
     <link rel="stylesheet" href="/build/assets/css/main.css">
