f2d85b05d553eece379fbb1236c95de6c289f9d6
PCDotFan <467055732@qq.com>
2016-10-09 15:06:20 +0800

    disable qiniu

--- app/Phphub/Presenters/UserPresenter.php
+++ app/Phphub/Presenters/UserPresenter.php
@@ -11,11 +11,14 @@ class UserPresenter extends Presenter
      */
     public function gravatar($size = 100)
     {
+        // 暂时关闭七牛。
+        /*
         if (config('app.url_static') && $this->avatar) {
             //Using Qiniu image processing service.
             $postfix = $size > 0 ? "?imageView2/1/w/{$size}/h/{$size}" : '';
             return cdn('uploads/avatars/'.$this->avatar) . $postfix;
         }
+        */
         $github_id = $this->github_id;
         $domainNumber = rand(0, 3);
         return "https://avatars{$domainNumber}.githubusercontent.com/u/{$github_id}?v=2&s={$size}";
